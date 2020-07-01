f09cb43472dbebbd67002214e741c81464dafbaa
Daniel Sperling <daniel_sperling@yahoo.com>
2020-07-01 20:40:59 +0200

    fixing the form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'products', to: 'pages#products'
-  get 'contact', to: 'pages#contact'
   resources :contacts, only: [:new, :create]
 end
