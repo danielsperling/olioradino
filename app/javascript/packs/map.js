4357cc39f2b47fb27319cd4e30fab80285dc5423
Daniel Sperling <daniel_sperling@yahoo.com>
2020-07-10 03:14:20 +0200

    added webpack and config for mapbox. Still does not show

---
+++ app/javascript/packs/map.js
@@ -0,0 +1,15 @@
+import mapboxgl from 'mapbox-gl';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/danielsperling/ckcf4r1dn17kd1inyikg0gzth'
+    });
+  }
+};
+
+export { initMapbox };
