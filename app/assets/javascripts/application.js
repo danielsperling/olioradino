9343b46dd93400531af76b634eef92cf8ec68300
Daniel Sperling <daniel_sperling@yahoo.com>
2020-10-09 18:47:56 +0300

    finish navabr implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,14 +21,14 @@
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li')
+  const navLinks = document.querySelectorAll('.nav-links li');
   // toogle Nav
   burger.addEventListener('click',() => {
     nav.classList.toggle('nav-active');
   });
   // Animate Links
-  navLInks.forEach((link, index) => {
-    link.style.animation = 'navLinkFade 0.5s ease forwards ${index /7}s';
+  navLinks.forEach((link, index) => {
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.5}s`;
   });
 }
 
